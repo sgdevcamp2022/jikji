b009b512a2cce20ea6357a8d34a0cc188a3a5219
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-02-11 00:34:35 +0900

    Feat : model, dto 구현

---
+++ backend/media-server/src/main/java/com/jikji/mediaserver/model/Media.java
@@ -0,0 +1,53 @@
+package com.jikji.mediaserver.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Media {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "media_id")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "media_type")
+	private MediaType mediaType;
+
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt = LocalDateTime.now();
+
+	private String url;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Builder
+	public Media(String url, User user, MediaType mediaType) {
+		this.mediaType = mediaType;
+		this.url = url;
+		this.user = user;
+	}
+
+}
